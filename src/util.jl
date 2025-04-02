e903d712e2297813137220763af16bc78d257129
Christopher David White <christopher.d.white117.ctr@us.navy.mil>
2025-04-02 08:23:54 -0400

    add some util functions

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,6 @@
 export @cassert
 export last_eigenindex_below
+export unzip
 
 # conditional assert
 # https://discourse.julialang.org/t/assert-alternatives/24775/14?u=cdwhite1
@@ -16,3 +17,8 @@ function normalize!(u)
   nu = norm(u)
   u ./= nu
 end
+
+scalar(A) = (if length(A) != 1; error("scalar: argument has wrong length A = $A"); else A[1]; end)
+unzip(A :: Array{Tuple{S,T}} where {T,S}) = ([a[1] for a in A], [a[2] for a in A])
+unzip(A :: Array{Tuple{R,S,T}} where {R,S,T}) = ([a[1] for a in A], [a[2] for a in A], [a[3] for a in A],)
+
